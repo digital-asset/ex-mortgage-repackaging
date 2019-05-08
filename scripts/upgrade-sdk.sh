b8106123fd1ee1ae9db2ee99b4d60b87b67a4076
Tamás Bártfai <lt@digitalasset.com>
2019-05-08 16:05:19 +0200

    Add SDK upgrade script

---
+++ scripts/upgrade-sdk.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
+
+fetch_latest_version() {
+    SDK_VERSION="$(curl -s -L -H "Accept: application/json"  https://github.com/digital-asset/daml/releases/latest | jq -r '.tag_name' | tr -d v)"
+}
+
+if [ $# -eq 0 ]
+then
+    echo "Fetching latest SDK version"
+    SDK_VERSION="$(curl -s -L -H "Accept: application/json"  https://github.com/digital-asset/daml/releases/latest | jq -r '.tag_name' | tr -d v)"
+elif [ $# -eq 1 ]
+then
+    SDK_VERSION=$1
+else
+    echo "Usage ${BASH_SOURCE[0]} [SDK_VERSION]"
+    exit 1
+fi
+
+echo "SDK version is $SDK_VERSION"
+echo "modifying ${PROJECT_DIR}/da.yaml"
+sed -e "s/sdk-version: .*$/sdk-version: $SDK_VERSION/" -i "${PROJECT_DIR}/da.yaml"
+
+echo "modifying ${PROJECT_DIR}/docker/sandbox.Dockerfile"
+sed -e "s/FROM digitalasset\/daml-sdk:.*$/FROM digitalasset\/daml-sdk:$SDK_VERSION-master/" -i "${PROJECT_DIR}/docker/sandbox.Dockerfile"
+
+echo "modifying ${PROJECT_DIR}/.circleci/config.yml"
+sed -e "s/image: digitalasset\/daml-sdk:.*$/image: digitalasset\/daml-sdk:$SDK_VERSION-circleci-master/" -i "${PROJECT_DIR}/.circleci/config.yml"
