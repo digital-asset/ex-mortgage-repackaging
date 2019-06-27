8d4cb502b9f73809456150f06bcce43914ae5937
Gabor Hosszu <gabor@digitalasset.com>
2019-06-27 11:46:26 +0200

    Remove old frontend

--- da-grpc/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { DaGrpc, IDaGrpc } from './da_grpc'
-export { Contract, IChoiceProto, IContractProto, IContractConstructor, createContract, fullName } from './contract'
-export { IDatatypeProto, IType, IDatatypeProtoList, IDatatypeProtoRecord, ILeafType } from './datatype'
-export { IFinalLogger, ILogger, ISimpleLogger, mkLogger } from './helpers'
--- frontend/src/contracts/MortgagePool/helpers.ts
+++
@@ -1,3 +0,0 @@
-export interface IWithUnderlyingCusips {
-  getUnderlyingCusips(): string[]
-}
